d66e5cb577683594e6eefb0eaea75f52e55c9439
NemanjaN2 <nemanja.najdanovic@hotmail.com>
2026-05-12 11:55:35 +0200

    Update __init__.py

--- najdangpt-api/app/services/__init__.py
+++ najdangpt-api/app/services/__init__.py
@@ -1,10 +1,9 @@
 """
 Business logic services
 """
-
 from app.services.rag import generate_embedding, chunk_text, retrieve_relevant_context, cosine_similarity
 from app.services.memory import get_conversation_memory, update_conversation_memory
-from app.services.search import search_web
+from app.services.search import search_web, fetch_url, extract_urls
 
 __all__ = [
     "generate_embedding",
@@ -14,4 +13,6 @@ __all__ = [
     "get_conversation_memory",
     "update_conversation_memory",
     "search_web",
+    "fetch_url",
+    "extract_urls",
 ]
